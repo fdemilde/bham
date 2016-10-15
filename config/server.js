652ea3f40f3d6b70e959bc8d06cdabdac17cc794
Jon Leibham <jleibham@gmail.com>
2016-10-15 16:58:06 -0500

    setup-routing

--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 var http = require('http');
+var url = require("url");
 
 function start() {
     function onRequest(req, res) {
+        var pathname = url.parse(req.url).pathname;
+        console.log("Request for " + pathname + " received.");
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         });
