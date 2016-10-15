649564ed6e6539d82aafc3dcb99c3548a7116d5f
Jon Leibham <jleibham@gmail.com>
2016-10-15 16:18:35 -0500

    setup

--- config/system.js
+++ config/system.js
@@ -3,7 +3,6 @@ var url = require("url");
 
 function start() {
     function onRequest(req, res) {
-      var pathname = url.parse(req.url).pathname;
       console.log("Request for " + pathname + " received.");
       res.writeHead(200, {"Content-Type": "text/plain"});
       res.write("Hello World");
